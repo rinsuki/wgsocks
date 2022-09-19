be1363ccc3e7a6da0edd1946cc6bd3ca7ba86c5f
rinsuki <428rinsuki+git@gmail.com>
2022-09-19 23:36:38 +0900

    Implement correct handle of unsupported address types

--- src/socks.rs
+++ src/socks.rs
@@ -77,39 +77,44 @@ fn handle_socks(socks_socket: TcpStream) -> std::io::Result<(TcpStream, smoltcp:
         panic!("Invalid SOCKS reserved byte");
     }
     // 10. Read address
-    let addr: Option<smoltcp::wire::IpAddress>;
-    match buf[3] {
+    let addr: Option<smoltcp::wire::IpAddress> = match buf[3] {
         1 => {
             // IPv4
             let mut buf = [0u8; 4];
             socks_socket.read_exact(&mut buf).unwrap();
-            addr = Some(smoltcp::wire::IpAddress::v4(buf[0], buf[1], buf[2], buf[3]));
+            Some(smoltcp::wire::IpAddress::v4(buf[0], buf[1], buf[2], buf[3]))
         },
         3 => {
             // Domain name
-            todo!()
+            println!("Currently DNS resolve is not supported");
+            None
         },
         4 => {
-            // IPv6
-            let mut buf = [0u8; 16];
-            socks_socket.read_exact(&mut buf).unwrap();
-            // ... is that works??
-            addr = Some(smoltcp::wire::IpAddress::v6(
-                (buf[0] as u16) | ((buf[1] as u16) << 8),
-                (buf[2] as u16) | ((buf[3] as u16) << 8),
-                (buf[4] as u16) | ((buf[5] as u16) << 8),
-                (buf[6] as u16) | ((buf[7] as u16) << 8),
-                (buf[8] as u16) | ((buf[9] as u16) << 8),
-                (buf[10] as u16) | ((buf[11] as u16) << 8),
-                (buf[12] as u16) | ((buf[13] as u16) << 8),
-                (buf[14] as u16) | ((buf[15] as u16) << 8),
-            ))
+            println!("Currently IPv6 dest is not supported");
+            None
+            // // IPv6
+            // let mut buf = [0u8; 16];
+            // socks_socket.read_exact(&mut buf).unwrap();
+            // // ... is that works??
+            // addr = Some(smoltcp::wire::IpAddress::v6(
+            //     (buf[0] as u16) | ((buf[1] as u16) << 8),
+            //     (buf[2] as u16) | ((buf[3] as u16) << 8),
+            //     (buf[4] as u16) | ((buf[5] as u16) << 8),
+            //     (buf[6] as u16) | ((buf[7] as u16) << 8),
+            //     (buf[8] as u16) | ((buf[9] as u16) << 8),
+            //     (buf[10] as u16) | ((buf[11] as u16) << 8),
+            //     (buf[12] as u16) | ((buf[13] as u16) << 8),
+            //     (buf[14] as u16) | ((buf[15] as u16) << 8),
+            // ))
         },
         _ => panic!("Invalid SOCKS address type: {}", buf[3]),
-    }
+    };
     let addr = match addr {
         Some(addr) => addr,
-        None => panic!("Invalid SOCKS address"),
+        None => {
+            socks_socket.write(&[5, 3, 0, 1, 0, 0, 0, 0, 0, 0]).unwrap();
+            return Err(std::io::Error::new(std::io::ErrorKind::Other, "Invalid address"));
+        },
     };
     // 12. Connect to address
     let port = {
